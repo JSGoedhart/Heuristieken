06ef58c1e6631e8cbc1df70eed325860ea298788
Lennert Jansen <lennertjansen@wcw-stud-145-109-37-7.wireless.uva.nl>
2017-04-19 12:21:15 +0200

    LJ1904_2
    Test

--- application_a.py
+++ application_a.py
@@ -1,40 +1,18 @@
 import csv
 import operator
 from classes import *
-# from helpers import *
-
-
-# # define class for cargo's
-# class cargo1:
-# 	def __init__(self, number, kg, m3):
-# 		self.number = number
-# 		self.kg = int(kg)
-# 		self.m3 = float(m3)
+from helpers import *
 
 # create list to put cargo1 classes in
-cargo1_list = []
-
-# put cargo-elements of list 1 in class cargo1
-csvfile = open('CargoList1.csv', 'r')
-for line in csvfile:
-	split = line.split(';')
-	var0 = split[0]
-	var1 = int(split[1])
-	var2 = float(split[2][:-1])
-	cargo1_list.append(cargo1(var0, var1, var2))
+cargo1_list = open_cargo_csv('CargoList1.csv')
 
 # sort cargo1_list's kg from high to low and create new sorted list
 cargo1_sorted = sorted(cargo1_list, key=operator.attrgetter('kg'), reverse=True)
 
-# # define class for spacecrafts
-# class spacecraft:
-# 	def __init__(self, name, kg, m3):
-# 		self.name = name
-# 		self.kg = kg
-# 		self.m3 = m3
-
 # create a list with the four spacecrafts put into classes in it
-spacecraft_list = [spacecraft('cygnus', 2000, 18.9), spacecraft('verne', 2300, 13.1), spacecraft('progress', 2400, 7.6), spacecraft('kounotori', 5200, 14)]
+#spacecraft_list = [spacecraft('cygnus', 2000, 18.9), spacecraft('verne', 2300, 13.1), spacecraft('progress', 2400, 7.6), spacecraft('kounotori', 5200, 14)]
+
+spacecraft_list = open_spacecrafts_csv('Spacecrafts.csv')
 
 # create lists to put cargo-classes in
 spacecrafts = [[], [], [], []]
--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
-def split_csv(file):
+from classes import *
+
+def open_cargo_csv(file):
     # create list to put cargo1 classes in
-    cargo1_list = []
+    split_list = []
 
     # put cargo-elements of list 1 in class cargo1
     csvfile = open(file, 'r')
@@ -9,37 +11,51 @@ def split_csv(file):
     	var0 = split[0]
     	var1 = int(split[1])
     	var2 = float(split[2][:-1])
-    	cargo1_list.append(cargo1(var0, var1, var2))
+    	split_list.append(cargo1(var0, var1, var2))
+    return split_list
 
-def fill_spacecrafts:
-    # create a list with the four spacecrafts put into classes in it
-    spacecraft_list = [spacecraft('cygnus', 2000, 18.9), spacecraft('verne', 2300, 13.1), spacecraft('progress', 2400, 7.6), spacecraft('kounotori', 5200, 14)]
+def open_spacecrafts_csv(file):
+    # create empty list for spacecrafts
+    open_list = []
 
-    # create lists to put cargo-classes in
-    spacecrafts = [[], [], [], []]
-
+    # put spacecrafts in empty list
     csvfile = open(file, 'r')
     for line in csvfile:
-    	split = line.split(';')
-    	var0 = split[0]
-    	var1 = int(split[1])
-    	var2 = float(split[2][:-1])
-    	cargo1_list.append(cargo1(var0, var1, var2))
-
-    # put cargo-items in list of spacecraft, when spacecraft is full, go to next
-    for j in range(0, 4):
-    	# define available mass in spacecraft
-    	mass_av = spacecraft_list[j].kg
-    	for i in range(0, len(cargo1_sorted)):
-    		# check if cargo-item is already placed
-    		if (cargo1_sorted[i].kg == 'nan'):
-    			next
-    		else:
-    			# put cargo-item in spacecraft if there is enough space
-    			if (cargo1_sorted[i].kg <= mass_av):
-    				spacecrafts[j].append(cargo1(cargo1_sorted[i].number, cargo1_sorted[i].kg, cargo1_sorted[i].m3))
-    				mass_av -= cargo1_sorted[i].kg
-    				cargo1_sorted[i].kg = 'nan'
-    			# when item doesn't fit, try next
-    			else:
-    				next
+        split = line.split(';')
+        var0 = split[0]
+        var1 = int(split[1])
+        var2 = float(split[2][:-1])
+        open_list.append(spacecraft(var0, var1, var2))
+    return open_list
+# def fill_spacecrafts:
+#     # create a list with the four spacecrafts put into classes in it
+#     spacecraft_list = [spacecraft('cygnus', 2000, 18.9), spacecraft('verne', 2300, 13.1), spacecraft('progress', 2400, 7.6), spacecraft('kounotori', 5200, 14)]
+#
+#     # create lists to put cargo-classes in
+#     spacecrafts = [[], [], [], []]
+#
+#     csvfile = open(file, 'r')
+#     for line in csvfile:
+#     	split = line.split(';')
+#     	var0 = split[0]
+#     	var1 = int(split[1])
+#     	var2 = float(split[2][:-1])
+#     	cargo1_list.append(cargo1(var0, var1, var2))
+#
+#     # put cargo-items in list of spacecraft, when spacecraft is full, go to next
+#     for j in range(0, 4):
+#     	# define available mass in spacecraft
+#     	mass_av = spacecraft_list[j].kg
+#     	for i in range(0, len(cargo1_sorted)):
+#     		# check if cargo-item is already placed
+#     		if (cargo1_sorted[i].kg == 'nan'):
+#     			next
+#     		else:
+#     			# put cargo-item in spacecraft if there is enough space
+#     			if (cargo1_sorted[i].kg <= mass_av):
+#     				spacecrafts[j].append(cargo1(cargo1_sorted[i].number, cargo1_sorted[i].kg, cargo1_sorted[i].m3))
+#     				mass_av -= cargo1_sorted[i].kg
+#     				cargo1_sorted[i].kg = 'nan'
+#     			# when item doesn't fit, try next
+#     			else:
+#     				next
