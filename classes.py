b7fe77a03354cfdf75b648a629804188cda80bec
Lennert Jansen <lennertjansen@re-stud-146-50-226-247.wireless.uva.nl>
2017-04-26 12:58:54 +0200

    hardcore weight and volume capacity in classes

--- classes.py
+++ classes.py
@@ -1,11 +1,14 @@
+weight_cap = 11895
+volume_cap = 72.05
+
 # define class for cargo's
 class cargo1:
 	def __init__(self, number, kg, m3):
 		self.number = number
 		self.kg = int(kg)
 		self.m3 = float(m3)
-		self.valkg = float(kg/11895)*100
-		self.valm3 = float(m3/72.05)*100
+		self.valkg = float(kg/weight_cap)*100
+		self.valm3 = float(m3/volume_cap)*100
 		self.valtot = self.valkg + self.valm3
 
 # define class for spacecrafts
