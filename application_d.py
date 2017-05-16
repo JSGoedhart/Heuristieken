3998007029bb3776df6c763e9023c64663364da9
Nik Gabel <nik.gabel@student.uva.nl>
2017-05-16 11:11:53 +0200

    update

--- application_d.py
+++ application_d.py
@@ -20,9 +20,9 @@ spacecrafts_kg = [[], [], [], [], [], []]
 
 # run greedy fill, to fill spacecrafts on basis of kg
 greedy_fill(spacecraft_list_sorted, cargo3_sorted_kg, spacecrafts_kg, 'kg', 'm3')
-print spacecraft_list_sorted
+
 # create lists to put cargo-classes in
-spacecrafts_m3 = []
+spacecrafts_fleet = []
 
 leftover_list = cargo3_sorted_m3
 count = 0
@@ -32,35 +32,86 @@ while len(leftover_list) != 0:
 	temp_cargo = leftover_list
 	greedy_fill(spacecraft_list_sorted, temp_cargo, temp_spacecrafts, 'm3', 'kg')
 	leftover_list = temp_spacecrafts[5]
+	spacecrafts_fleet.append(temp_spacecrafts)
 
-	# print the leftoverlist
-	# print 'leftoverlist:', count
-	# for i in range(len(leftover_list)):
-	# 	print leftover_list[i].m3
+# # prints all cargo stored in spacecrafts
+# for k in range(len(spacecrafts_m3)):
+# 	print 'spacecrafts:', k
+# 	for j in range(5):
+# 		print spacecraft_list[j].name
+# 		for i in range(len(spacecrafts_m3[k][j])):
+# 			print spacecrafts_m3[k][j][i].m3
 
-	spacecrafts_m3.append(temp_spacecrafts)
+score = 0
 
-# for i in range(len(spacecrafts_m3)):
-# 	print sum_m3(spacecrafts_m3[i])
-
-
-
-
-
-# prints all cargo stored in spacecrafts
-for k in range(len(spacecrafts_m3)):
-	print 'spacecrafts:', k
+# score function:
+for k in range(len(spacecrafts_fleet)):
 	for j in range(5):
-		print spacecraft_list[j].name
-		for i in range(len(spacecrafts_m3[k][j])):
-			print spacecrafts_m3[k][j][i].m3
+		kg = 0
+		m3 = 0
+		for i in range(len(spacecrafts_fleet[k][j])):
+			kg = kg + spacecrafts_fleet[k][j][i].kg
+			m3 = m3 + spacecrafts_fleet[k][j][i].m3
+		weighted_kg = kg/spacecraft_list_sorted[j].kg
+		weighted_m3 = m3/spacecraft_list_sorted[j].m3
+		score = score + weighted_m3 + weighted_kg
 
+print score
 
 # prints leftover with all NaN
 # for i in range(count):
 # 	print 'leftoverlist:', i
 # 	for j in range(len(spacecrafts_m3[i][6])):
 # 		print spacecrafts_m3[i][6][j].m3
+
+# random swappen
+
+number_fleet = range(len(spacecrafts_fleet))
+
+# select fleet
+rand_fleet = random.choice(number_fleet)
+number_spacecrafts = range(len(spacecrafts_fleet[rand_fleet]) - 1)
+
+# select spacecraft
+rand_spacecraft = random.choice(rand_fleet)
+
+# select item
+rand_item = random.choice(rand_spacecraft)
+
+# select fleet 2
+rand_fleet2 = random.choice(spacecrafts_fleet)
+
+# select spacecraft 2
+rand_spacecraft2 = random.choice(rand_fleet2)
+
+# select item 2
+rand_item2 = random.choice(rand_spacecraft2)
+
+spacecrafts_fleet[rand_fleet][rand_spacecraft][rand_item]
+
+# swap itemns
+spacecrafts_fleet[rand_fleet][rand_spacecraft][rand_item], spacecrafts_fleet[rand_fleet2][rand_spacecraft2][rand_item2] = swap(spacecrafts_fleet[rand_fleet][rand_spacecraft][rand_item], spacecrafts_fleet[rand_fleet2][rand_spacecraft2][rand_item2])
+
+spacecrafts_fleet[rand_fleet][rand_spacecraft][rand_item]
+
+# staat dit allemaal al in een functie?????????????
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
