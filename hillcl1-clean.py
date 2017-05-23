34280b295241550152ce4594b6bfc24d3654bdf8
JSGoedhart <joosjegoedhart@gmail.com>
2017-05-23 16:10:01 +0200

    hillclimbing 2 in function

--- hillcl1-clean.py
+++ hillcl1-clean.py
@@ -9,19 +9,22 @@ import plotly.graph_objs as go
 import numpy
 
 # run main function with cargolist, greedy kg and algorithm: hillclimbing1 for 2 seconds
-xy = main('CargoList1.csv', greedy_fill, hillclimbing1, 'kg', 2);
-
-# put x, y -values in array
-score = xy[0]; x=xy[1]
+hillcl1 = main('CargoList1.csv', greedy_fill, hillclimbing1, 'm3', 2);
+hillcl2 = main('CargoList1.csv', greedy_fill, hillclimbing2, 'm3', 2);
 
 # TEST
 trace1 = go.Scatter(
-	name = 'greedy kg',
-	x = x,
-	y = score)
+	name = 'Hillclimber 1',
+	x = hillcl1[1],
+	y = hillcl1[0])
 
-layout = go.Layout(
-	title = 'Hillclimbing 1, greedy kg and m3',
+trace2 = go.Scatter(
+	name = 'Hillclimber 2',
+	x = hillcl2[1],
+	y = hillcl2[0])
+
+layout = go.Layout(	
+	title = 'List: cargo 1, starting point: greedy m3',
 	width=500,
 	height= 400,
 	yaxis = dict(
@@ -41,8 +44,8 @@ layout = go.Layout(
 	legend=dict(x=0.7, y=1.0)
 	)
 
-data = [trace1]
+data = [trace1, trace2]
 
 fig = go.Figure(data=data, layout=layout)
 
-py.iplot(fig, filename='Greedy Hillclimbing 1, cargo 1')
+py.iplot(fig, filename='Plot 2')
