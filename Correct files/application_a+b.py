fb8c1f4b4b8d844f1ff508288870e5ce86691ec0
JSGoedhart <joosjegoedhart@gmail.com>
2017-05-26 12:17:43 +0200

    Map with correct files

---
+++ Correct files/application_a+b.py
@@ -0,0 +1,48 @@
+import csv
+import operator
+import classes
+from helpers import *
+
+# create list to put cargo1 classes in
+cargo1_list_kg = open_cargo_csv('CargoList1.csv')
+cargo1_list_m3 = open_cargo_csv('CargoList1.csv')
+
+# sort cargo1_list's kg from high to low and create new sorted list
+cargo1_sorted_kg = sorted(cargo1_list_kg, key=operator.attrgetter('kg'), reverse=True)
+cargo1_sorted_m3 = sorted(cargo1_list_m3, key=operator.attrgetter('m3'), reverse=True)
+
+# create a list with the four spacecrafts put into classes in it
+spacecraft_list = open_spacecrafts_csv('Spacecrafts.csv')
+
+# create lists of spacecrafts to put cargo-classes in
+spacecrafts_kg = [[], [], [], [], []]
+
+# define global
+LEN = len(spacecrafts_kg)
+
+# run greedy fill, to fill spacecrafts on basis of kg
+# greedy_fill(spacecraft_list, cargo1_sorted_kg, spacecrafts_kg, 'kg', 'm3')
+greedy_fill(spacecraft_list, cargo1_sorted_kg, spacecrafts_kg, 'kg', 'm3')
+
+
+# print output
+print 'Question a: output on basis of kg:'
+print 'Spacecrafts:   ',  print_names(spacecraft_list)
+print 'Total kg in spacecrafts:', sum_kg(spacecrafts_kg)
+print 'Total m3 in spacecrafts:', sum_m3(spacecrafts_kg)
+print 'Score:', val_leftover(spacecrafts_kg[LEN-1])
+print ''
+
+# create lists of spacescrafts to put cargo-classes in
+spacecrafts_m3 = [[], [], [], [], []]
+
+# run greedy fill, to fill spacecrafts on basis of m3
+greedy_fill(spacecraft_list, cargo1_sorted_m3, spacecrafts_m3, 'm3', 'kg')
+
+# print output
+print 'Question b: output on basis of m3:'
+print 'spacecrafts:',  print_names(spacecraft_list)
+print 'total kg in spacecrafts:', sum_kg(spacecrafts_m3)
+print 'total m3 in spacecrafts:', sum_m3(spacecrafts_m3)
+print 'score:', val_leftover(spacecrafts_m3[LEN-1])
+print ''
---
+++ Correct files/main-plot.py
@@ -0,0 +1,31 @@
+import csv
+import operator
+import random
+import time
+import classes
+from helpers import *
+import plotly.plotly as py
+import plotly.graph_objs as go
+import numpy
+
+# create array with algorithms that should be used, with corresponding coolingschemes if necessary
+algorithms = [hillclimbing1, hillclimbing2, annealing1, annealing1, annealing2, annealing2]
+coolingscheme = [False, False, 'exponential', 'sigmoidal', 'exponential', 'sigmoidal']
+# assign legendnames to each algorithm
+legend = ['Hillclimber 1', 'Hillclimber 2', 'Annealing 1 - Exp', 'Annealing 1 - Sigmd', 'Annealing 2 - Exp', 'Annealing 2 - Sigm']
+
+# create array to put x- and y-values of each algorithm in
+datalist = []
+
+# run each algorithm and save results
+for i in range(len(algorithms)):
+	# Divide items of cargolist with startingpoint greedy, than run the selected algorithm for the selected time
+	output = main('CargoList1.csv', greedy_fill, algorithms[i], coolingscheme[i], 'm3', 2);
+	# append legendname and x,y-values to datalist
+	datalist.append([legend[i], output[1], output[0]])
+
+# put datalist in plotable format
+data = createdata(datalist)
+
+# plot the results of all the used algorithms, with selected range and size
+plot('Cargo 2 with starting point greedy m3', [25, 40], data, 1000, 600, 'Main plot')
